bb7d1e74e46afc0d4264b4b86d2c6172b195b27a
Ueli Krapf <ueli.krapf@gmx.net>
2018-07-22 10:53:49 +0200

    1. Versuch CSV Parser

--- app/src/main/java/com/example/gauss/modularbeit/MainActivity.java
+++ app/src/main/java/com/example/gauss/modularbeit/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.gauss.modularbeit;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        AssetManager assetManager = getAssets();
+
+        try {
+            InputStream csvStream = assetManager.open("mis.csv");
+            InputStreamReader csvStreamReader = new        InputStreamReader(csvStream);
+            CSVReader csvReader = new CSVReader(csvStreamReader,'\t');
+            String[] line;
+
+            // throw away the header
+            csvReader.readNext();
+
+            while ((line = csvReader.readNext()) != null) {
+                for (String value :line){
+                    Log.e("test", value);
+                }
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
